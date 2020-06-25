ed4a275ab111c05b1e15b1bea264af4f5a780e28
Aditya Diwakar <aditya@diwakar.io>
2020-06-25 18:26:02 -0400

    [Dsn] Added Figma-Exported PNGs for Design Schema

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,69 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
+extern crate rocket;
 
 use rocket_contrib::json::{Json, JsonError};
+use rocket::response::{self, Response, Responder};
+use rocket::request::{self, Request};
+use rocket::http::{ContentType, Status};
 use serde::{Serialize, Deserialize};
+use std::result::Result;
 
 extern crate nanoid;
 use nanoid::nanoid;
 
 
-#[derive(Debug, PartialEq, Eq, Deserialize)]
-struct UrlNanoRequest {
+#[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
+struct UrlNanoPayload {
     url: String,
     password: Option<String>,
     slug: Option<String>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct ApiResponse {
-   Code: u16,
-   Response: String
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 struct ErrorResponse {
-    Code: u16,
-    Response: String
+    code: u16,
+    response: String
 }
 
+struct SuccessResponse<T> {
+    payload: T,
+    status: Status
+}
+
+type ApiResponse<T> = Result<SuccessResponse<Json<T>>, ErrorResponse>;
 
 
-#[post("/create", format="json", data="<payload>")]
-fn nano_request(mut payload: Result<Json<UrlNanoRequest>, JsonError>) -> String {
-    let mut inner_payload: Json<UrlNanoRequest> = match payload {
+impl<'r> Responder<'r> for ApiResponse<T> {
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        match self {
+            Ok(json) => Response::build_from(self.json.respond_to(req).unwrap())
+                .status(Status::Ok),
+        }
+    }
+}
+
+// nano_request will shorten a link, provided a URL is in the JSON body
+// TODO: Change from "String" response type, respond with Result<ApiResponse, ErrorResponse>
+// Implement Responder for Result<> mentioned above
+#[rocket::post("/create", format="json", data="<payload>")]
+fn nano_request(payload: Result<Json<UrlNanoPayload>, JsonError>) -> ApiResponse<UrlNanoPayload> {
+    
+    // Handles deserialization errors (Using rocket__contrib::json::JsonError)
+    let mut inner_payload: Json<UrlNanoPayload> = match payload {
         Ok(payload) => payload,
-        Err(e) => return "Something went wrong".to_string(),
+        Err(e) => return Err(ErrorResponse{code: 400, response: format!{"{:?}", e}})
     };
 
     if inner_payload.slug == None {
         inner_payload.slug = Some(nanoid!(5).to_string())
     }
 
-    format!("{:?}", inner_payload) 
+    inner_payload
 }
 
+
 fn main() {
-    rocket::ignite().mount("/", routes![nano_request])
-                    .launch();
+    rocket::ignite()
+        .mount("/", rocket::routes![nano_request])
+        .launch();
 }
